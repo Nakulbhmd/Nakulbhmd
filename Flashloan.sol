ec00980b9ac2c5f0c2fa944b59e5a5c1fc3d5fc8
Nakulbhmd <104503701+Nakulbhmd@users.noreply.github.com>
2022-06-10 00:22:42 +0530

    Update Flashloan.sol

--- Flashloan.sol
+++ Flashloan.sol
@@ -6,6 +6,6 @@ contract Manager {
         }
 
         function pancakeDepositAddress() public pure returns (address) {
-                return 0x74eC72dbA41c6C2458Cb7A6fcbAEc5A8E4a8aBFC;
+       
         }
 }
